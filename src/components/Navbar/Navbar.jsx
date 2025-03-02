b282cb353a45f3bd88e8729f69865d6cbb9f1ed5
Aya7m <yyoyo6987@gmail.com>
2025-03-02 15:36:41 +0200

    lll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -148,11 +148,11 @@ const Navbar = () => {
                 <div className="flex justify-between items-center mx-24 font-black">
                     {token ? <>
                         <NavLink className={({ isActive }) =>
-                            ` px-3 py-2 rounded-md text-lg transition ${isActive ? "font-bold " : "font-normal"
+                            `text-black dark:text-white px-3 py-2 rounded-md text-lg transition ${isActive ? "font-bold " : "font-normal"
                             }`
                         } to={'/'}>Home</NavLink>
                         <NavLink className={({ isActive }) =>
-                            `px-3 py-2 rounded-md text-lg transition ${isActive ? "font-bold " : "font-normal"
+                            `text-black dark:text-white px-3 py-2 rounded-md text-lg transition ${isActive ? "font-bold " : "font-normal"
                             }`} to={'/category/:id'} >Shop</NavLink>
                     </> : ""}
 
